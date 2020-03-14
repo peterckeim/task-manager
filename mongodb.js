d01f00a632d7449df37f9a9c9ae747e396f88d47
peterckeim <peterckeim@users.noreply.github.com>
2020-03-13 20:57:38 -0400

    Implement updateMany challenge with promise returned.

--- mongodb.js
+++ mongodb.js
@@ -14,74 +14,17 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ _id: new ObjectID("5e6b1a75bfd3bb07ac461138") }, (error, result) => {
-    //     if (error) {
-    //         return console.log('unable to fetch')
-    //     }
-
-    //     console.log(result)
-
-    // })
-
-    db.collection('tasks').findOne({ _id : new ObjectID('5e6b1b6c3257983088c931eb')}, (error, result) => {
-        if (error) {
-            return console.log('error fetching result')
+    db.collection('tasks').updateMany({
+        completed : false
+    }, {
+        $set: {
+            completed : true
         }
-        console.log(result)
+    }).then((result) => {
+        console.log(result.modifiedCount, 'Records Updated!')
+    }).catch((error) => {
+        console.log(error)
     })
-
-    db.collection('tasks').find({ completed: false }).toArray((error, result) => {
-        if (error) {
-            return console.log('problem finding')
-        }
-        console.log(result)
-    })
-
-    // db.collection('users').find({ age: 27 }).toArray((error, result) => {
-    //     if (error) {
-    //         return console.log('problem finding')
-    //     }
-    //     console.log(result)
-    // })
-
-    // db.collection('users').find({ age: 27 }).count((error, result) => {
-    //     if (error) {
-    //         return console.log('problem finding')
-    //     }
-    //     console.log(result)
-    // })
-
-
-
-    // db.collection('users').insertOne({
-    //         name: 'Vikram',
-    //         age: 22
-    //     }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Problem inserting documents')
-    //     }
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Clean sink',
-    //         completed: false
-    //     }, {
-    //         description: 'Clean bar',
-    //         completed: false
-    //     }, {
-    //         description: 'Clean yard',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Problem inserting documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
 
 })
 

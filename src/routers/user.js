6a199ea1094c1bfbe3fc03f73db64c4186a635d5
peterckeim <peterckeim@users.noreply.github.com>
2020-03-15 03:29:12 -0400

    create authentication middleware, set up authentication on requesting user information, implement bcrypt on all created and mofified passwords, add jwt for token handling to keep users logged in across multiple devices for a set time

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,23 +1,31 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
-// Create a new user
+// Create a new user (credentials on body in JSON)
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (e) { res.status(400).send(e) }
 })
 
-// GET all users
-router.get('/users', async (req, res) => {
+// Login a user (credentials on body in JSON)
+router.post('/users/login', async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) { res.status(500).send() }
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) { res.status(400).send() }
+})
+
+// GET logged in user.
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 // GET a user by ID
@@ -40,9 +48,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id,
-                                                  req.body,
-                                                  { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
         if (!user) return res.status(404).send()
         res.send(user)
     } catch (e) { 
